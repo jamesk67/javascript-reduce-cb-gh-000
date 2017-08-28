b22d5c21de89babd5d11ee8e092fb31ffdb46fe8
James Kim <jamesgki.kim67@gmail.com>
2017-08-28 01:17:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 const app = "I don't do much."
+
+const products = [
+  { name: 'Head & Shoulders Shampoo', price: 4.99, discount: .6 },
+  { name: 'Twinkies', price: 7.99, discount: .45 },
+  { name: 'Oreos', price: 6.49, discount: .8 },
+  { name: 'Jasmine-scented bath pearls', price: 13.99, discount: .7 },
+];
+
+function getTotalAmountForProducts(products, callback, initialValue) {
+  let totalPrice = initialValue;
+  products.forEach(product => {
+    if (product.discount >= 0.5) {
+      callback(totalPrice, product);
+    }
+  });
+
+  return totalPrice;
+}
+
+function callback(totalPrice, product) {
+  if (product.price < 7) {
+    return totalPrice + product.price;
+  }
+  return totalPrice;
+}
+
+console.log(getTotalAmountForProducts(products, callback, 0));
+
+function reduce(collection, callback, initialValue) {
+  let result = initialValue;
+  collection.forEach(product => {
+    result = callback(result, product, index, collection);
+  });
+  return result;
+}
+
+const couponLocations = [
+  { room: 'Living room', amount: 5 },
+  { room: 'Kitchen', amount: 2 },
+  { room: 'Bathroom', amount: 1 },
+  { room: 'Master bedroom', amount: 7 },
+];
+
+function couponCounter(totalAmount, location) {
+  return totalAmount + location.amount;
+}
+
+console.log(reduce(couponLocations, couponCounter, 0));
+console.log(couponLocations.reduce(couponCounter, 0));
